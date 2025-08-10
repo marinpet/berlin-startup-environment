43e398072221e1e0208e1096367e01183a6738cb
marinpet <marina.55kovic@gmail.com>
2025-08-10 23:02:46 +0200

    add visuals dir

--- config.py
+++ config.py
@@ -10,6 +10,5 @@ ROOT_DIR = Path(__file__).resolve().parent
 DATA_DIR = ROOT_DIR / "data"
 RAW_DIR = DATA_DIR / "raw"
 PROCESSED_DIR = DATA_DIR / "processed"
-
-
+VISUALS_DIR = DATA_DIR / "visuals"
 
